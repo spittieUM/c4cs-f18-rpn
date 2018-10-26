cc22dfa8fae0411525495aa717bf838bbd3d8403
spittie <spittie@umich.edu>
2018-10-26 11:16:26 -0400

    Wrote calculate function for addition

--- rpn.py
+++ rpn.py
@@ -2,7 +2,22 @@
 
 
 def calculate(arg):
-    pass
+
+    stack = []
+
+    tokens = arg.split()
+
+    for token in tokens:
+        try:
+            value = int(token)
+            stack.append(value)
+        except ValueError:
+            val1 = stack.pop()
+            val2 = stack.pop()
+            result = val1 + val2
+            stack.append(result)
+
+            return stack[0]
 
 
 def main():
