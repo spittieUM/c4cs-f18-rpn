9dfc2f568b0338d2cac30712d27856ff0dbb9379
spittie <spittie@umich.edu>
2018-10-26 11:32:26 -0400

    Added tests for mul and div

--- test_rpn.py
+++ test_rpn.py
@@ -13,3 +13,13 @@ class TestBasics(unittest.TestCase):
     def test_sub(self):
         result = rpn.calculate('4 3 -')
         self.assertEqual(1, result)
+
+
+    def test_mul(self):
+        result = rpn.calculate("2 3 *")
+        self.assertEqual(6, result)
+
+
+    def test_div(self):
+        result = rpn.calculate('2 4 /')
+        self.assertEqual(2, result)
