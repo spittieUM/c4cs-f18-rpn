a4c611e003eb0ff741a8700e505309bca9a65f63
spittie <spittie@umich.edu>
2018-10-26 11:49:03 -0400

    Fixed bugs in rpn. All tests pass.

--- rpn.py
+++ rpn.py
@@ -22,14 +22,15 @@ def calculate(arg):
             stack.append(value)
 
         except ValueError:
-            val2 = stack.pop()
-            val1 = stack.pop()
+            val2 = int(stack.pop())
+            val1 = int(stack.pop())
 
             func = op[token]
             result = func(val1, val2)
 
             stack.append(result);
-            return stack[0]
+            print (stack[0])
+            return int(stack[0])
 
 
 def main():
