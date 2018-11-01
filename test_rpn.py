2581ccf144632d3cc85e148074eadbbf04098543
spittie <spittie@umich.edu>
2018-11-01 19:43:09 -0400

    Wrote test for ^

--- test_rpn.py
+++ test_rpn.py
@@ -23,3 +23,8 @@ class TestBasics(unittest.TestCase):
     def test_div(self):
         result = rpn.calculate('4 2 /')
         self.assertEqual(2, result)
+
+
+    def test_pow(self):
+        result = rpn.calculate('3 2 ^')
+        self.assertEqual(9, result)
