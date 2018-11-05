effaf7a7b9b5a13a8f18150ce4a1b40179688d5c
spittie <spittie@umich.edu>
2018-11-04 22:17:45 -0500

    Fixed small error in tests

--- test_rpn.py
+++ test_rpn.py
@@ -7,7 +7,7 @@ class TestBasics(unittest.TestCase):
 
     def test_add(self):
         result = rpn.calculate("1 1 +")
-       self.assertEqual(2, result)
+        self.assertEqual(2, result)
 
 
     def test_sub(self):
